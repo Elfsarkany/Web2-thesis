65fdcf63471b2084813bb8f58e1c2ce79e376222
Bodgál Attila Zoltán <elf.sarkany@gmail.com>
2025-05-05 19:51:15 +0200

    authorization and basic user setup

--- Beadando/DnD_character_creator_and_modifier/backend/routes/userRoute.js
+++ Beadando/DnD_character_creator_and_modifier/backend/routes/userRoute.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 const router = express.Router();
 
 const User = require("./models/User.js");
+const UserProfile = require("./models/UserProfile.js");
+const e = require('express');
 
 router.post("/signup", async (req, res) => {
     try{
@@ -17,10 +17,56 @@ router.post("/signup", async (req, res) => {
             return res.status(500).json({ message: "Email is already in use"});
         }
         const hashedPassword = await bcrypt.hash(password, 10);
+        const newUser = new User({
+            email,
+            password: hashedPassword,
+        });
+
+        await newUser.save();
+        const newUserProfile = new UserProfile({
+            email,
+            email,
+        });
+        await newUserProfile.save();
         
+        res.status(201).json({message: 'User created succesfully.'});
+    } catch (error){
+        console.error('Error during signup', error);
+        res.status(500).json({message: 'Server error during signup'});
     }
 });
 
+router.post("/signin", async (req, res) => {
+    try{
+        const {email, password} = req.body;
 
+        const user = await User.findOne({email});
+
+        if(!user){
+            return res.status(400).json({message: 'Invalid email or password'});
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if(!isMatch){
+            return res.status(400).json({message: 'Invalid email or password'});
+        }
+
+        const token = jwt.sign({
+            userId: user._id,
+            email: user.email,
+        }, 
+        process.env.JWT_SECRET, {
+            expiresIn: '1h'
+        });
+
+        const profile = await UserProfile.findOne({email});
+
+        res.json({token, profile});
+    } catch (error){
+        console.error('Error during signin', error);
+        res.status(500).json({message: 'Server error during signin'});
+    }
+});
 
 module.exports = router;
--- Beadando/DnD_character_creator_and_modifier/backend/server.js
+++ Beadando/DnD_character_creator_and_modifier/backend/server.js
@@ -3,13 +3,31 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv = require('dotenv');
 
-
-
 dotenv.config();
 
-const userRoute = require('./routes/userRoute');
-const characterRoute = require('./routes/characterRoute');
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect(process.env.MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(()=>{
+    console.log("Connected to Database");
+})
+.catch((err) =>{
+    console.error("Database connection error:", err);
+})
+
+app.listen(PORT, ()=>{
+    console.log('Linstening on: ' + PORT);
+}, );
+
+//const userRoute = require('./routes/userRoute');
+//const characterRoute = require('./routes/characterRoute');
 
 
 
-app.use("/api/auth", auth);
+//app.use("/api/auth", userRoute);
