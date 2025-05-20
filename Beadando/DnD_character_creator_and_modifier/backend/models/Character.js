dff118ef6d29efeef6bf66a2c66515287a9df139
Bodgál Attila Zoltán <elf.sarkany@gmail.com>
2025-05-20 15:59:25 +0200

    really the last for models

--- Beadando/DnD_character_creator_and_modifier/backend/models/Character.js
+++ Beadando/DnD_character_creator_and_modifier/backend/models/Character.js
@@ -1,21 +1,28 @@
 const mongoose = require("mongoose");
+const Feat = require("./Feat");
+const CharacterClass = require("./CharacterClass");
+const SubClass = require("./SubClass");
+const Lineage = require("./Lineage");
+const Background = require("./Background");
+const Spell = require("./Spell");
+const Item = require("./Item");
 
 const characterSchema = new mongoose.Schema({
     owner: {type: String, required: true},
     name: {type: String, required: true},
-    background: {type: String, required: true},
-    lineage: {type: String, required: true},
-    class: {type: String, required: true},
-    subClass: {type: String},
+    background: {type: Background, required: true},
+    lineage: {type: Lineage, required: true},
+    class: {type: CharacterClass, required: true},
+    subClass: {type: SubClass},
     level: {type: Number, min:1, max:20, required: true},
     baseStats: {type: [Number], required: true},
     stats: {type: [Number], required: true},
     proficiencis: {type: [Boolean]},
     expertises: {type: [Boolean]},
-    feats: {type: [String]},
+    feats: {type: [Feat]},
     visions: {type: [Number]},
-    chosenSpells: {type: [String]},
-    items: {type: [String]},
+    chosenSpells: {type: [Spell]},
+    items: {type: [Item]},
     biography: {
         name: {type: String},
         age: {type: String},
