b1c9884bf5c9e85fc2a98b33b7de123ff8745155
Bodgál Attila Zoltán <elf.sarkany@gmail.com>
2025-06-03 07:53:35 +0200

    services and componenets generated

--- Beadando/DnD_character_creator_and_modifier/frontend/src/app/app.routes.ts
+++ Beadando/DnD_character_creator_and_modifier/frontend/src/app/app.routes.ts
@@ -3,11 +3,20 @@ import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
 import { ProfileComponent } from './components/profile/profile.component';
 import { authGuard } from './guards/auth.guard';
+import { WikiListComponent } from './components/wiki/wiki-list/wiki-list.component';
+import { CharacterCreateComponent } from './components/character/character-create/character-create.component';
+import { CharacterListComponent } from './components/character/character-list/character-list.component';
+import { WikiCreateComponent } from './components/wiki/wiki-create/wiki-create.component';
 
 export const routes: Routes = [
-    {path:'', redirectTo:'login', pathMatch:'full'},
+    {path:'', redirectTo:'wiki', pathMatch:'full'},
     {path: 'login', component: LoginComponent},
     {path: 'signup', component: SignupComponent},
     {path: 'profile', component: ProfileComponent, canActivate: [authGuard]},
-    {path: '**', redirectTo: 'login'},
+    {path: 'wiki', component: WikiListComponent},
+    {path: 'wiki/create', component: WikiCreateComponent, canActivate: [authGuard]},
+    {path: 'character', component: CharacterListComponent, canActivate: [authGuard]},
+    {path: 'character/create', component: CharacterCreateComponent, canActivate: [authGuard]},
+    {path: 'character/edit/:characterID', component: CharacterCreateComponent, canActivate: [authGuard]},
+    {path: '**', redirectTo: 'wiki'},
 ];
---
+++ Beadando/DnD_character_creator_and_modifier/frontend/src/app/components/wiki/wiki-create/wiki-create.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-wiki-create',
+  imports: [],
+  templateUrl: './wiki-create.component.html',
+  styleUrl: './wiki-create.component.css'
+})
+export class WikiCreateComponent {
+
+}
