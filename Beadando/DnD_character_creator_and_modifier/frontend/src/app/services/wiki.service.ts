1c0e3f9fd3c695855846078d8c402720a327c51e
Bodgál Attila Zoltán <elf.sarkany@gmail.com>
2025-06-06 19:21:19 +0200

    create wikicomponent

--- Beadando/DnD_character_creator_and_modifier/frontend/src/app/services/wiki.service.ts
+++ Beadando/DnD_character_creator_and_modifier/frontend/src/app/services/wiki.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { environment } from '../../environments/environment';
 import { Feat } from '../components/wiki/wiki.models/feat';
+import { Spell } from '../components/wiki/wiki.models/spell';
 
 @Injectable({
   providedIn: 'root'
@@ -11,8 +12,19 @@ export class WikiService {
 
   constructor (private http: HttpClient, private router: Router) { }
 
-  addFeat(feat: Feat){
-    const body = {what: 'feat', feat};
+
+  getMasters(){
+    
+  }
+
+  addWikiComponent(what: string, name: string, publication: string, origin: string, feats: Feat[], requirementLvl: number,
+    statBonuses: number[], skillProficiencis: boolean[], skillExpertises: boolean[], skillSavingProficiencis: boolean[],
+    weaponProficiencis: boolean[], spells: Spell[], visions: number[], weapontype: string, school: string, 
+    components: string, duration: string, range: string, target: string, description: string){
+
+    const body = {what, name, publication, origin, feats, requirementLvl, statBonuses, skillProficiencis, skillExpertises,
+      skillSavingProficiencis, weaponProficiencis,spells, visions, weapontype, school, components, duration, range, target,description
+    };
     return this.http.post('${environment.apiUrl}/wiki/create', body );
   }
 
